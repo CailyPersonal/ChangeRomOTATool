f8300bcc2499688a302b6dfa2b210a62530cc8fd
caily <wuqi@terminus.io>
2018-01-23 10:05:24 +0800

    add login view with test

---
+++ app/src/main/java/caily/jiaoye/com/changeromotatool/LogInStatus.kt
@@ -0,0 +1,9 @@
+package caily.jiaoye.com.changeromotatool
+
+/**
+ * Created by caily on 22/01/2018.
+ */
+enum class LogInStatus {
+    LogIn,
+    LogOut;
+}
--- app/src/main/java/caily/jiaoye/com/changeromotatool/MainActivity.kt
+++ app/src/main/java/caily/jiaoye/com/changeromotatool/MainActivity.kt
@@ -1,12 +1,39 @@
 package caily.jiaoye.com.changeromotatool
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+       /* when(checkLogInStats()) {
+            LogInStatus.LogIn -> TODO()
+            LogInStatus.LogOut -> {
+                setContentView(R.layout.activity_login)
+            }
+        }*/
+
+
+
         setContentView(R.layout.activity_main)
+
+
+        jump_test.setOnClickListener {
+            run {
+                val intent = Intent().apply {
+                    this.setClass(this@MainActivity, caily.jiaoye.com.changeromotatool.view.LoginActivity::class.java)
+                }
+                this.startActivity(intent)
+            }
+        }
+    }
+
+    fun checkLogInStats(): LogInStatus {
+
+        return LogInStatus.LogOut
     }
 }
